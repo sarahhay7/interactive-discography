b937825e043e1a1fc2649220008e7c0d70d1966f
Sarah Hay <sarah.hay@trineo.com>
2017-07-20 13:29:09 +1200

    show difference between jsx and es6

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import FilterableSongList from './filterable-song-list';
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
+    // <FilterableSongList songs={songs} />,
     React.createElement(FilterableSongList, {songs: songs}, null),
     document.getElementById('mount')
   );
